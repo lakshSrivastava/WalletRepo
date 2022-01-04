cefeaad3ce0cc14e056c7dadcd44560e34a9e069
Laksh Srivastava <69974330+lakshSrivastava@users.noreply.github.com>
2022-01-04 09:39:43 +0530

    Update WalletConstants.java

--- wallet/src/main/java/com/assessment/wallet/constants/WalletConstants.java
+++ wallet/src/main/java/com/assessment/wallet/constants/WalletConstants.java
@@ -4,13 +4,13 @@ public class WalletConstants {
 
 	public static final String NON_EXISTING_USER = "You are not in our records, please sign-up to create a new Account";
 	
-	public static final int PASSWORD_MIN_LEN = 5;
+	public static final int PASSWORD_MIN_LEN = 3;
 	
-	public static final int PASSWORD_MAX_LEN = 15;
+	public static final int PASSWORD_MAX_LEN = 10;
 	
-	public static final String TRANSACTION_TYPE_ADD_MONEY = "A";
+	public static final String TRANSACTION_TYPE_ADD_MONEY = "ADD";
 	
-	public static final String TRANSACTION_TYPE_TRANSFER_MONEY = "T";
+	public static final String TRANSACTION_TYPE_TRANSFER_MONEY = "TRANS";
 	
 	public static final String ADD_MONEY_GREATER_THAN_ZERO = "Please add money greater than 0";
 	
